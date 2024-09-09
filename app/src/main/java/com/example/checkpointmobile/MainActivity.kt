a984358cb60d8f3ecd557f27c153ae1d3911edcf
yoboypb <pedro_araujo0812@hotmail.com>
2024-09-09 06:00:21 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/checkpointmobile/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.checkpointmobile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.checkpointmobile.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Home())
+                R.id.Search -> replaceFragment(Search())
+                R.id.Library -> replaceFragment(Library())
+
+                else ->{
+
+                }
+
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+}
